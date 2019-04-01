57217df62201762a0e235f759f82e11255899e5e
Ki Song <kiwisquash@gmail.com>
2019-04-01 13:32:28 -0700

    Updates to index and detail functions in polls views.py to use templates.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,24 @@
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Question
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = "<br> ".join([str(i+1)+". "+ q.question_text for i, q in enumerate(latest_questions)])
-    return HttpResponse("Here are five most recent questions: <br><br>" + output)
+    # latest_questions = []  
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_questions': latest_questions,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    response = f"You're looking at question {question_id}."
-    return HttpResponse(response)
+    question = Question.objects.get(pk=question_id)
+    template = loader.get_template('polls/detail.html')
+    context = {
+        'question': question,
+    }
+    return HttpResponse(template.render(context, request))
 
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}."
