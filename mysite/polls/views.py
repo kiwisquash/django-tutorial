0df97fc51dbe4fb7d845546b87b2e3c747ac40b9
Ki Song <kiwisquash@gmail.com>
2019-04-01 10:53:42 -0700

    Added detail, results, and vote views into views.py annd polls.urls module.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,3 +2,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world! I am learning to use Django.")
+
+def detail(request, question_id):
+    response = f"You're looking at question {question_id}."
+    return HttpResponse(response)
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    response = f"You're voting on question {question_id}."
+    return HttpResponse(response)
+
