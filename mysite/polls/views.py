5a7c5e1703a5de4307c72fde8a6774c01433070a
Ki Song <kiwisquash@gmail.com>
2019-04-01 11:33:16 -0700

    Index page for polls now displays 5 most recent questions.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world! I am learning to use Django.")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = "<br> ".join([str(i+1)+". "+ q.question_text for i, q in enumerate(latest_questions)])
+    return HttpResponse("Here are five most recent questions: <br><br>" + output)
 
 def detail(request, question_id):
     response = f"You're looking at question {question_id}."
