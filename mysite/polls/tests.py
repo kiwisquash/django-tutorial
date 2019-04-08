55a046198f32ecb975e7bec0a9c413f6c65dc502
Ki Song <kiwisquash@gmail.com>
2019-04-08 14:53:16 -0700

    Added tests for was_published_recently() method for all possible date cases. Also modified the test names so that the names reflect the type of date being tested.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -7,7 +7,7 @@ from .models import Question
 
 class QuestionModelTests(TestCase):
 
-    def test_fish(self):
+    def test_was_published_recently_with_future_question(self):
         '''
         was_published_recently() returns False for
         questions whose pub_date is in the future.
@@ -17,4 +17,25 @@ class QuestionModelTests(TestCase):
         future_q = Question(pub_date=time)
 
         self.assertIs(future_q.was_published_recently(), False)
-# Create your tests here.
+
+    def test_was_published_recently_with_old_question(self):
+        '''
+        was_published_recently() returns False for
+        questions whose pub_date is older than 1 day.
+        '''
+
+        time = timezone.now() - dt.timedelta(days=1,seconds=1)
+        old_q = Question(pub_date=time)
+
+        self.assertIs(old_q.was_published_recently(), False)
+        
+    def test_was_published_recently_with_recent_question(self):
+        '''
+        was_published_recently() returns True for
+        questions whose pub_date is within the last 1 day.
+        '''
+
+        time = timezone.now() - dt.timedelta(hours=23,minutes=59,seconds=59)
+        recent_q = Question(pub_date=time)
+
+        self.assertIs(recent_q.was_published_recently(), True)
